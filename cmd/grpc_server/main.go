d765134cd698750a20fa9f51f69c3ac06a54dbb4
kenyako <kenyako_1103@mail.ru>
2024-03-21 13:36:54 +0700

    added DI container

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -2,179 +2,21 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"net"
-	"time"
 
-	sq "github.com/Masterminds/squirrel"
-	"github.com/brianvoe/gofakeit"
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgxpool"
-	desc "github.com/kenyako/chat-server/pkg/chat_v1"
-
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
-	"google.golang.org/protobuf/types/known/emptypb"
+	"github.com/kenyako/chat-server/internal/app"
 )
-
-const (
-	grpcPort = 50051
-	dbDSN    = "host=localhost port=5433 dbname=chat-service user=chat-user password=chat-password sslmode=disable"
-)
-
-type server struct {
-	desc.UnimplementedChatAPIServer
-
-	db *pgxpool.Pool
-	qb sq.StatementBuilderType
-}
-
-func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
-
-	chatTitle := req.GetTitle()
-	members := req.GetUsernames()
-
-	builderInsert := s.qb.Insert("chats").
-		Columns("title").
-		Values(chatTitle).
-		Suffix("RETURNING id")
-
-	query, args, err := builderInsert.ToSql()
-	if err != nil {
-		log.Fatalf("failed to build query: %v", err)
-	}
-
-	rows, err := s.db.Query(ctx, query, args...)
-	if err != nil {
-		log.Fatalf("failed to get rows: %v", err)
-	}
-	defer rows.Close()
-
-	chatID, err := pgx.CollectOneRow(rows, pgx.RowTo[int64])
-	if err != nil {
-		log.Fatalf("failed to get chatID: %v", err)
-	}
-
-	builderInsert = s.qb.Insert("users_chats").
-		Columns("chat_id", "user_id")
-
-	// генерация ID пользователей, позже будет добавлена ручка для получения ID из базы auth
-	for _, username := range members {
-		userID := int64(gofakeit.Uint8()) + int64(len(username))
-
-		log.Printf("user with id: %d added in chat", userID)
-
-		builderInsert = builderInsert.Values(chatID, userID)
-	}
-
-	query, args, err = builderInsert.ToSql()
-	if err != nil {
-		log.Fatalf("failed to build query: %v", err)
-	}
-
-	_, err = s.db.Exec(ctx, query, args...)
-	if err != nil {
-		log.Fatalf("failed to added data in db: %v", err)
-	}
-
-	return &desc.CreateResponse{
-		Id: chatID,
-	}, nil
-}
-
-func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
-
-	chatID := req.GetId()
-
-	builderDelete := s.qb.Delete("users_chats").
-		Where(sq.Eq{"chat_id": chatID})
-
-	query, args, err := builderDelete.ToSql()
-	if err != nil {
-		log.Fatalf("failed to biuld delete query UsersChats: %v", err)
-	}
-
-	_, err = s.db.Exec(ctx, query, args...)
-	if err != nil {
-		log.Fatalf("failed to delete UsersChats: %v", err)
-	}
-
-	builderDelete = s.qb.Delete("chat_messages").
-		Where(sq.Eq{"chat_id": chatID})
-
-	query, args, err = builderDelete.ToSql()
-	if err != nil {
-		log.Fatalf("failed to biuld delete query Messages: %v", err)
-	}
-
-	_, err = s.db.Exec(ctx, query, args...)
-	if err != nil {
-		log.Fatalf("failed to delete Messages: %v", err)
-	}
-
-	builderDelete = s.qb.Delete("chats").
-		Where(sq.Eq{"id": chatID})
-
-	query, args, err = builderDelete.ToSql()
-	if err != nil {
-		log.Fatalf("failed to biuld delete query Chats: %v", err)
-	}
-
-	_, err = s.db.Exec(ctx, query, args...)
-	if err != nil {
-		log.Fatalf("failed to delete Chats: %v", err)
-	}
-
-	return &emptypb.Empty{}, nil
-}
-
-func (s *server) SendMessage(ctx context.Context, req *desc.SendRequest) (*emptypb.Empty, error) {
-
-	chatID := req.GetChatID()
-	from := req.GetFrom()
-	text := req.GetText()
-
-	builderInsert := s.qb.Insert("chat_messages").
-		Columns("chat_id", "user_id", "text", "time_sent").
-		Values(chatID, from, text, time.Now())
-
-	query, args, err := builderInsert.ToSql()
-	if err != nil {
-		log.Fatalf("failed to build insert Messages: %v", err)
-	}
-
-	_, err = s.db.Exec(ctx, query, args...)
-	if err != nil {
-		log.Fatalf("failed to insert Messages: %v", err)
-	}
-
-	return &emptypb.Empty{}, nil
-}
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	ctx := context.Background()
 
-	pool, err := pgxpool.New(ctx, dbDSN)
+	a, err := app.NewApp(ctx)
 	if err != nil {
-		log.Fatalf("failed to create pool: %v", err)
-	}
-	defer pool.Close()
-
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("failed to create new App: %v", err)
 	}
 
-	s := grpc.NewServer()
-	reflection.Register(s)
-	desc.RegisterChatAPIServer(s, &server{
-		db: pool,
-		qb: sq.StatementBuilder.PlaceholderFormat(sq.Dollar),
-	})
-
-	if err = s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	err = a.Run()
+	if err != nil {
+		log.Fatalf("failed to run gRPC server: %v", err)
 	}
 }
---
+++ internal/app/service_provider.go
@@ -0,0 +1,115 @@
+package app
+
+import (
+	"context"
+	"log"
+
+	"github.com/kenyako/chat-server/internal/client/db"
+	"github.com/kenyako/chat-server/internal/client/db/pg"
+	"github.com/kenyako/chat-server/internal/closer"
+	"github.com/kenyako/chat-server/internal/config"
+	"github.com/kenyako/chat-server/internal/config/env"
+	"github.com/kenyako/chat-server/internal/repository"
+	"github.com/kenyako/chat-server/internal/service"
+
+	chatAPI "github.com/kenyako/chat-server/internal/api/chat"
+	chatRepo "github.com/kenyako/chat-server/internal/repository/chat"
+	chatServ "github.com/kenyako/chat-server/internal/service/chat"
+)
+
+type serviceProvider struct {
+	pgConfig   config.PGConfig
+	grpcConfig config.GRPCConfig
+
+	dbClient db.Client
+
+	chatRepository repository.ChatAPIRepo
+	chatService    service.ChatAPIService
+
+	chatImpl *chatAPI.Implementation
+}
+
+func newServiceProvider() *serviceProvider {
+	return &serviceProvider{}
+}
+
+func (s *serviceProvider) PGConfig() config.PGConfig {
+
+	if s.pgConfig == nil {
+
+		cfg, err := env.NewPgConfig()
+		if err != nil {
+			log.Fatalf("failed to get pg config: %v", err)
+		}
+
+		s.pgConfig = cfg
+	}
+
+	return s.pgConfig
+}
+
+func (s *serviceProvider) GRPCConfig() config.GRPCConfig {
+
+	if s.grpcConfig == nil {
+
+		cfg, err := env.NewGRPCConfig()
+		if err != nil {
+			log.Fatalf("failed to get gRPC config: %v", err)
+		}
+
+		s.grpcConfig = cfg
+	}
+
+	return s.grpcConfig
+}
+
+func (s *serviceProvider) DBCClient(ctx context.Context) db.Client {
+
+	if s.dbClient == nil {
+
+		client, err := pg.New(ctx, s.PGConfig().DSN())
+		if err != nil {
+			log.Fatalf("failed to create dbc client: %v", err)
+		}
+
+		err = client.DB().Ping(ctx)
+		if err != nil {
+			log.Fatalf("ping db failed: %v", err)
+		}
+		closer.Add(client.Close)
+
+		s.dbClient = client
+	}
+
+	return s.dbClient
+}
+
+func (s *serviceProvider) ChatRepository(ctx context.Context) repository.ChatAPIRepo {
+
+	if s.chatRepository == nil {
+
+		s.chatRepository = chatRepo.NewRepository(s.DBCClient(ctx))
+	}
+
+	return s.chatRepository
+}
+
+func (s *serviceProvider) ChatService(ctx context.Context) service.ChatAPIService {
+
+	if s.chatService == nil {
+
+		s.chatService = chatServ.NewServiceChat(s.ChatRepository(ctx))
+	}
+
+	return s.chatService
+}
+
+func (s *serviceProvider) ChatImpl(ctx context.Context) *chatAPI.Implementation {
+
+	if s.chatImpl == nil {
+
+		s.chatImpl = chatAPI.NewImplementation(s.ChatService(ctx))
+	}
+
+	return s.chatImpl
+}
